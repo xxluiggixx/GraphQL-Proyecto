c6a8ee0ce7d5c05d980913d083ff2cd2df36d802
Luiggi <lgarciasaladokuhl@outlook.com>
2018-10-12 17:15:43 -0300

    revisar las versiones q dan error en casa

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 /* REQUERIR DEPENDENCIAS*/
 const express = require('express')
 const bodyParser = require('body-parser')
+const {graphqlExpress, graphiqlExpress} = require('graphql-server-express') //es un middle word
+const schema = require('./schema') //importo el schema de graphql
 
 const app = express()//Server express
+
+//app.use("que endPoint", Lo pasamos por bodyParser, middel world q importamos)
+app.use(
+  '/graphql',
+  bodyParser.json(),
+  graphqlExpress({schema})
+)
+//poder utilizar la parte grafica
+app.use(
+  '/graphql',
+  graphiqlExpress({
+    endpointURL:'/graphql'
+  })
+)
+
 const PORT = 3000
 
-app.get('/', function (req,res) {
-  res.send('Hello World!')
-})
+
 // Para q empieze a escuchar un puerto
 app.listen(PORT, () => {
   console.log('Servidor corriendo OK');
--- schema.js
+++ schema.js
@@ -1,9 +1,19 @@
+//importar un tools para crear un schema ejecutable
 const {makeExecutableSchema}= require('graphql-tools')
 
-
-const schema = `
+const typeDefs = `
   type Curso {
     id: ID!
     titulo: String!
   }
+
+  type Query{
+    cursos: [Curso]
+  }
 `
+//generar un schema a traves de la herramienta
+const schema = makeExecutableSchema({
+  typeDefs
+})
+
+module.exports = schema
